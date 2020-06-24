ad155c6dad918339c6bed9892e64c0950ad8a678
Nckflannery <Nckflannery@users.noreply.github.com>
2020-06-24 13:40:38 -0700

    Decode recursive method not quite working still

--- Day-3-Challenges/Decode String/DecodeString.py
+++ Day-3-Challenges/Decode String/DecodeString.py
@@ -16,3 +16,25 @@ class Solution:
         # for i, j in zip(nums, chars):
         #     out += (int(i) * j)
         # return out 
+
+    # # I think we need a recursive solution
+    # # Attempt 2 Still not working :(
+    #   nums = {}
+    #   chars = {}
+    #   out_string = ''
+      
+    #   self.recurser(s, nums, chars)
+      
+    #   for i, j in zip(nums.values(), chars.values()):
+    #     out_string += int(i) * j
+    #   return out_string
+    
+    # def recurser(self, li: list, nums: dict, chars: dict):
+    #   for i, j in enumerate(li):
+    #     if isinstance(li, list):
+    #       recurser(j, nums, chars)
+    #     else:
+    #       if j.isdigit():
+    #         nums[i] = j
+    #       if j.isalpha():
+    #         chars[i] = j
