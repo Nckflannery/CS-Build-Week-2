c4eb699d584f3d9d05597e06555eb3fd1ceb4160
Nckflannery <Nckflannery@users.noreply.github.com>
2020-06-25 11:11:12 -0700

    DecodeString complete

--- Day-3-Challenges/Decode String/DecodeString.py
+++ Day-3-Challenges/Decode String/DecodeString.py
@@ -37,4 +37,45 @@ class Solution:
     #       if j.isdigit():
     #         nums[i] = j
     #       if j.isalpha():
-    #         chars[i] = j
+    #         chars[i] = j 
+
+
+    ## Solution: Thanks to Michelle Sirimanivong
+        stack = []
+        
+        # loop through string
+        for i in range(len(s)):
+            
+            # append characters that are not []
+            # if ], loop through characters in brackets to add to decoded
+            if s[i] == ']':
+                
+                # create variable to store decoded characters
+                decoded = ''
+                
+                # loop through stack if it's not [
+                while stack[-1] != '[':
+                    
+                    # pop last char from stack and add to decoded
+                    decoded = stack.pop() + decoded
+                    
+                # pop last char in stack, which should be [
+                stack.pop()
+                
+                # find k
+                k = ''
+                # while there is a stack and the last item of the stack is a number
+                while stack and stack[-1].isdigit():
+                    # add that to k
+                    k = stack.pop() + k
+                
+                # multiply k with decoded & add to stack
+                decoded = int(k) * decoded
+                stack.append(decoded)
+                
+            # add all characters into stack
+            else:
+                stack.append(s[i])
+                
+        # join decoded to make new string
+        return ''.join(stack)
