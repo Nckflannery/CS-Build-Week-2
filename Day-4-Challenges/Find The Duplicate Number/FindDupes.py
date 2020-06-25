c48ea9f4c6023c908f95629a61471dbc03096bae
Nckflannery <Nckflannery@users.noreply.github.com>
2020-06-25 14:46:46 -0700

    Solution wasn't O(1) space complexity

--- Day-4-Challenges/Find The Duplicate Number/FindDupes.py
+++ Day-4-Challenges/Find The Duplicate Number/FindDupes.py
@@ -10,12 +10,17 @@ class Solution:
         #   if len(lookup[i]) > 1:
         #     return i
 
-        # Slightly better solution (no list)
-        lookup = {}
-        for i in nums:
-          if i in lookup:
-            lookup[i] = 1
-          else:
-            lookup[i] = 0
-          if lookup[i] == 1:
-            return i
+        ## Not O(1) space complexity
+        # lookup = {}
+        # for i in nums:
+        #   if i in lookup:
+        #     lookup[i] = 1
+        #   else:
+        #     lookup[i] = 0
+        #   if lookup[i] == 1:
+        #     return i
+        
+        nums.sort()
+        for i in range(1, len(nums)):
+          if nums[i] == nums[i - 1]:
+            return nums[i]
